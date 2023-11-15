14ce71cbfb5c3686e561261a6baa10af0687b1f8
unknown <ds4950@naver.com>
2023-11-16 02:54:59 +0900

    aside 수정

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,27 +9,25 @@ import { useSelector } from 'react-redux'
 function Contact() {
 
   const darkMode = useSelector(state => state.darkMode);
-  const [isVisible, setIsVisible] = useState(false);
-
-  const toggleVisibility = () => {
-    if (window.pageYOffset > 300) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-  };
-
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  };
-
+  const [btnActive, setBtnActive] = useState(false);
+  const Topscroll = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  }
   useEffect(() => {
-    window.addEventListener("scroll", toggleVisibility);
-    return () => window.removeEventListener("scroll", toggleVisibility);
-  }, []);
+    const Button = () => {
+      const scrollY = window.scrollY;
+      if (scrollY > 350) {
+        setBtnActive(true);
+      } else {
+        setBtnActive(false);
+      }
+    };
+
+    window.addEventListener("scroll", Button)
+    return () => {
+      window.removeEventListener("scroll", Button)
+    }
+  }, [])
 
   return (
     <>
@@ -42,7 +40,7 @@ function Contact() {
                 <a href='https://github.com/flysofts' target='_blank'>
                   <FontAwesomeIcon icon={faGithub} className='text-4xl mr-5 dark:text-white' />
                 </a>
-                <a href='mailto:ds4950@naver.com' target='_blank'>
+                <a href='mailto:ds4950@naver.com' target='_blank' rel='noopener noreferrer'>
                   <FontAwesomeIcon icon={faEnvelope} className='text-4xl mr-5 dark:text-white' />
                 </a>
                 <a href="https://open.kakao.com/o/sUXFD3Rf" target='_blank'>
@@ -52,12 +50,15 @@ function Contact() {
               <p className='text-center text-xl pb-10'>저의 포트폴리오를 봐주셔서 감사합니다.</p>
               <div className='relative'>
 
-              <div className=' text-center text-xl font-pretty mt-20 text-white bg-black dark:bg-neutral-700'>&copy;2023 PARKJIHAWN. all rights reserved.</div>
-              <div onClick={scrollToTop}>
-          <FontAwesomeIcon icon={faArrowUp} className='absolute w-7 h-7 -top-24 right-12 cursor-pointer hover:bg-slate-300 hover:text-neutral-500 border-2 dark:bg-stone-700 dark:border-0  dark:hover:bg-red-200 dark:hover:text-gray-500 py-4 px-4 rounded-full '/>
-        </div>
+                <div className=' text-center text-xl font-pretty mt-20 text-white bg-black dark:bg-neutral-700'>&copy;2023 PARKJIHAWN. all rights reserved.</div>
+                {
+                  btnActive &&
+                  <>
+                    <div onClick={Topscroll}><FontAwesomeIcon icon={faArrowUp} className='fixed right-16 bottom-20  w-6 h-6 px-3 py-3 rounded-full border-2 cursor-pointer dark:border-0 dark:bg-red-100 dark:text-black' /></div>
+                  </>
+                }
               </div>
-            </div>         
+            </div>
           </div>
         </div>
       </div>
